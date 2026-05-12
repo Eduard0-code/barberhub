74823c64f7ce9004e809beee64386c87c2b0224f
Eduardo <eduardocs0900@gmail.com>
2026-05-12 13:13:41 -0300

    Mockup de cadastro e login

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import BarberCadLog from './components/Cadastro-Login-Barber.jsx'
 import './App.css'
 
 function App() {
