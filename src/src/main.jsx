89ba5306369c743c648f4b07ebec6334971b1f97
Álan <xalanchristian@outlook.com>
2026-05-12 13:31:35 -0300

    Alterações na main

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import BarberCadLog from './components/Cadastro-Login-Barber.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <BarberCadLog />
+    <App />
   </StrictMode>,
 )
